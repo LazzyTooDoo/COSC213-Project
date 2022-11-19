b02c993a3d027c348bcb2db4ec7954a14380493b
Lazar Todorovic <lazar.todorce@gmail.com>
2022-11-19 00:15:14 -0800

    Dummy description lol

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-<a href="index.html">Go To Home</a><br>
+<a id="home" href="index.html">Go To Home</a><br><br>
 <?php
 $mysqli = mysqli_connect("localhost", "laztodo", "malazaris", "testDB");
 
@@ -6,12 +6,23 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 } else {
+    if (!emtpy($_POST)) {
+        $sql = "INSERT INTO Users VALUES ('".$_POST["fname"].", ".$_POST["lname"].", ".$_POST["email"].", ".$_POST["phone"]."')";
+        $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+
+        if ($res === TRUE) {
+            echo "A record has been inserted.";
+        } else {
+            printf("Could not insert record: %s\n", mysqli_error($mysqli));
+        }
+        mysqli_free_result($res);
+    }
     $sql = "SELECT * FROM Users;";
     $res = mysqli_query($mysqli, $sql)or die (mysqli_error($mysqli));
 
     if (mysqli_num_rows($res) > 0) {
         ?>
-        <table style="border: 1px solid black">
+        <table">
             <tr>
                 <th><h2>First Name</h2></th>
                 <th><h2>Last Name</h2></th>
@@ -21,8 +32,8 @@ if (mysqli_connect_errno()) {
 
         <?php
         while ($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-            $firstName  = $newArray['fname'];
-            $lastName = $newArray['lname'];
+            $firstName  = $newArray['first_name'];
+            $lastName = $newArray['last_name'];
             $email = $newArray['email'];
             $phone = $newArray['phone'];
             ?>
