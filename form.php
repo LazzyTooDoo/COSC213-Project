4bc7cdea3760a7b73d772601aef7e3ce8750936d
Lazar Todorovic <lazar.todorce@gmail.com>
2022-11-19 00:17:30 -0800

    Dummy description lol

--- form.php
+++ form.php
@@ -6,44 +6,44 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 } else {
-    if (!empty($_POST)) {
-        $sql = "INSERT INTO Users VALUES ('".$_POST["fname"].", ".$_POST["lname"].", ".$_POST["email"].", ".$_POST["phone"]."')";
-        $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
-
-        if ($res === TRUE) {
-            echo "A record has been inserted.";
-        } else {
-            printf("Could not insert record: %s\n", mysqli_error($mysqli));
-        }
-        mysqli_free_result($res);
-    }
+//    if (!empty($_POST)) {
+//        $sql = "INSERT INTO Users VALUES ('".$_POST["fname"].", ".$_POST["lname"].", ".$_POST["email"].", ".$_POST["phone"]."')";
+//        $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+//
+//        if ($res === TRUE) {
+//            echo "A record has been inserted.";
+//        } else {
+//            printf("Could not insert record: %s\n", mysqli_error($mysqli));
+//        }
+//        mysqli_free_result($res);
+//    }
     $sql = "SELECT * FROM Users;";
-    $res = mysqli_query($mysqli, $sql)or die (mysqli_error($mysqli));
+    $res = mysqli_query($mysqli, $sql) or die (mysqli_error($mysqli));
 
     if (mysqli_num_rows($res) > 0) {
         ?>
         <table">
-            <tr>
-                <th><h2>First Name</h2></th>
-                <th><h2>Last Name</h2></th>
-                <th><h2>Email</h2></th>
-                <th><h2>Phone Number</h2></th>
-            </tr>
+        <tr>
+            <th><h2>First Name</h2></th>
+            <th><h2>Last Name</h2></th>
+            <th><h2>Email</h2></th>
+            <th><h2>Phone Number</h2></th>
+        </tr>
 
         <?php
         while ($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-            $firstName  = $newArray['first_name'];
+            $firstName = $newArray['first_name'];
             $lastName = $newArray['last_name'];
             $email = $newArray['email'];
             $phone = $newArray['phone'];
             ?>
-                <tr>
-                    <td><?php echo $firstName ?></td>
-                    <td><?php echo $lastName ?></td>
-                    <td><?php echo $email ?></td>
-                    <td><?php echo $phone ?></td>
-                </tr>
-                <?php
+            <tr>
+                <td><?php echo $firstName ?></td>
+                <td><?php echo $lastName ?></td>
+                <td><?php echo $email ?></td>
+                <td><?php echo $phone ?></td>
+            </tr>
+            <?php
         }
     } else {
         printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
